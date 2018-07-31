d59dfe65e84a305a65a13d080a1d416fb1f67710
keegan <keeganobonyo@gmail.com>
2018-07-31 10:25:48 +0300

    fixes

--- models/setup.sql
+++ models/setup.sql
@@ -1,5 +1,4 @@
 drop table sessions;
--- drop table users;
 
 
 create table users (
@@ -8,13 +7,5 @@ create table users (
   name       varchar(255),
   email      varchar(255) not null unique,
   password   varchar(255) not null,
-  created_at timestamp not null   
-);
-
-create table sessions (
-  id         serial primary key,
-  uuid       varchar(64) not null unique,
-  email      varchar(255),
-  user_id    integer references users(id),
   created_at timestamp not null   
 );
