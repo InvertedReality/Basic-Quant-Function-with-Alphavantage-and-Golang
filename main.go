4dc25b5fc115f0b934df342fdf79a5e5a38560a6
keegan <keeganobonyo@gmail.com>
2018-07-31 14:05:14 +0300

    functionality added for auto retrieving of url paths and serving on home page

--- main.go
+++ main.go
@@ -7,22 +7,25 @@ import (
     "github.com/gorilla/mux"
 )
 
+var path_details []string
 
 func main() {
 	p("Basic QUANT APP", version(), "started at Adress:", config.Address, "\n", time.Now())
-	router := mux.NewRouter()
+
+    router := mux.NewRouter()
 
 	//The urls
-	router.HandleFunc("/", Home)
-	router.HandleFunc("/get/data/", ValidationMiddleware(getdata))
-	router.HandleFunc("/get/meta/", ValidationMiddleware(GetMetaData))
-	router.HandleFunc("/get/graph/", ValidationMiddleware(getgraph))
-	router.HandleFunc("/user/logout/", ValidationMiddleware(authenticate))
-	router.HandleFunc("/user/auth/", authenticate)
-	router.HandleFunc("/user/signup/",ValidationMiddleware(UserExec))
-	router.HandleFunc("/user/list/",ValidationMiddleware(UserExec))
-	router.HandleFunc("/user/delete/",ValidationMiddleware(UserExec))
-	//Server details
+	router.HandleFunc("/", Home).Name("Home")
+	router.HandleFunc("/get/data/", ValidationMiddleware(getdata)).Name("data")
+	router.HandleFunc("/get/meta/", ValidationMiddleware(GetMetaData)).Name("meta_data")
+	router.HandleFunc("/get/graph/", ValidationMiddleware(getgraph)).Name("get_graph")
+	router.HandleFunc("/user/logout/", ValidationMiddleware(authenticate)).Name("logout")
+	router.HandleFunc("/user/auth/", authenticate).Name("authenticate")
+	router.HandleFunc("/user/signup/",ValidationMiddleware(UserExec)).Name("signup")
+	router.HandleFunc("/user/list/",ValidationMiddleware(UserExec)).Name("user_list")
+	router.HandleFunc("/user/delete/",ValidationMiddleware(UserExec)).Name("user_delete")
+    router.Walk(WalkFunc)
+
 	server := &http.Server{
 		Addr:           config.Address,
 		Handler:        router,
@@ -37,26 +40,18 @@ func main() {
 //home
 //lists urls
 func Home(writer http.ResponseWriter, request *http.Request) {
-	urls := map[int]string{
-		1: "/get/data",
-		2: "/get/meta",
-		3: "/get/graph",
-		4: "/user/logout",
-		5: "/user/signup",
-		6: "/user/auth/",
-		7: "/user/list/",
-		8: "/user/delete/",
-		9: "/user/auth/",
-	}
-
 	{
 		writer.Header().Set("Content-Type", "application/json")
 		writer.WriteHeader(http.StatusOK)
 		encoder := json.NewEncoder(writer)
 		encoder.SetIndent(empty, tab)
-		encoder.Encode(urls)
-
+		encoder.Encode(path_details)
 	}
 
 }
 
+func WalkFunc(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+    url,_:=route.URLPath()
+    path_details= append(path_details,url.Path)
+    return nil
+}
